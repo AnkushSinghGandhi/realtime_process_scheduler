5ea609e072eaa4594cf4e9f5f3bd95a88fe97a09
Ankush Singh Gandhi <ankushsinghgandhi@gmail.com>
2024-03-18 19:12:47 +0530

    implemented rms & edf scheduler algo

---
+++ src/rms_scheduler.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+class RMScheduler:
+    def __init__(self):
+        self.current_time = 0
+        self.ready_queue = deque()
+        self.running_task = None
+
+    def schedule(self, tasks):
+        self.ready_queue.extend(tasks)
+
+        print("Time\tTask")
+        while self.ready_queue:
+            self.ready_queue = deque(sorted(self.ready_queue, key=lambda x: x.deadline))
+            task = self.ready_queue.popleft()
+            if task.arrival_time > self.current_time:
+                self.current_time = task.arrival_time
+            print(f"{self.current_time}\t{task.name}")
+            self.current_time += 1
+            if self.current_time > task.deadline:
+                print(f"{task.name} missed deadline!")
+            else:
+                task.arrival_time += task.arrival_time
+                self.ready_queue.append(task)
+        print("Scheduling completed.")
