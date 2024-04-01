5d588828f120e1112120ed8196ae2ff2e69873a3
Ankush Singh Gandhi <ankushsinghgandhi@gmail.com>
2024-04-01 17:14:58 +0530

    rms algo updated

--- src/rms_scheduler.py
+++ src/rms_scheduler.py
@@ -4,22 +4,28 @@ class RMScheduler:
     def __init__(self):
         self.current_time = 0
         self.ready_queue = deque()
-        self.running_task = None
+        self.running_process = None
 
-    def schedule(self, tasks):
-        self.ready_queue.extend(tasks)
+    def schedule(self, processes):
+        if not processes:
+            return "No processes to schedule"  # Return a message if there are no processes
 
-        print("Time\tTask")
+        self.ready_queue.extend(processes)
+
+        scheduling_output = "Time\tProcess\n"
+
         while self.ready_queue:
             self.ready_queue = deque(sorted(self.ready_queue, key=lambda x: x.deadline))
-            task = self.ready_queue.popleft()
-            if task.arrival_time > self.current_time:
-                self.current_time = task.arrival_time
-            print(f"{self.current_time}\t{task.name}")
+            process = self.ready_queue.popleft()
+            if process.arrival_time > self.current_time:
+                self.current_time = process.arrival_time
+            scheduling_output += f"{self.current_time}\t{process.name}\n"
             self.current_time += 1
-            if self.current_time > task.deadline:
-                print(f"{task.name} missed deadline!")
+            if self.current_time > process.deadline:
+                scheduling_output += f"{process.name} missed deadline!\n"
             else:
-                task.arrival_time += task.arrival_time
-                self.ready_queue.append(task)
-        print("Scheduling completed.")
+                process.arrival_time += process.arrival_time
+                self.ready_queue.append(process)
+        
+        scheduling_output += "Scheduling completed.\n"
+        return scheduling_output
