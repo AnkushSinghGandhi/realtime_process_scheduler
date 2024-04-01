d69d5810317151954f9532ce0d37472aab5fa062
Ankush Singh Gandhi <ankushsinghgandhi@gmail.com>
2024-04-01 17:13:55 +0530

    edf algo fix

--- src/edf_scheduler.py
+++ src/edf_scheduler.py
@@ -4,22 +4,23 @@ class EDFScheduler:
     def __init__(self):
         self.current_time = 0
         self.ready_queue = deque()
-        self.running_task = None
+        self.running_process = None
 
-    def schedule(self, tasks):
-        self.ready_queue.extend(tasks)
+    def schedule(self, processes):
+        if not processes:
+            return "No processes to schedule"  # Return a message if there are no processes
 
-        print("Time\tTask")
+        self.ready_queue.extend(processes)
+
+        scheduling_output = "Time\tProcess\n"
+
         while self.ready_queue:
-            self.ready_queue = deque(sorted(self.ready_queue, key=lambda x: x.deadline))
-            task = self.ready_queue.popleft()
-            if task.arrival_time > self.current_time:
-                self.current_time = task.arrival_time
-            print(f"{self.current_time}\t{task.name}")
-            self.current_time += 1
-            if self.current_time > task.deadline:
-                print(f"{task.name} missed deadline!")
-            else:
-                task.arrival_time += task.arrival_time
-                self.ready_queue.append(task)
-        print("Scheduling completed.")
+            self.ready_queue = deque(sorted(self.ready_queue, key=lambda x: x.arrival_time))
+            process = self.ready_queue.popleft()
+            if process.arrival_time > self.current_time:
+                self.current_time = process.arrival_time
+            scheduling_output += f"{self.current_time}\t{process.name}\n"
+            self.current_time += process.burst_time  # Progress time by burst time
+        
+        scheduling_output += "Scheduling completed.\n"
+        return scheduling_output
